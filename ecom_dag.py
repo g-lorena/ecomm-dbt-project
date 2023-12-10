7ab28a2e1af28797f3991d7f4a361bd2aef56846
lgongang <lorena.gongang.partner@decathlon.com>
2023-12-10 21:50:14 +0100

    added docker_conn_id and api_version to test the load of the dag

--- ecom_dag.py
+++ ecom_dag.py
@@ -15,10 +15,9 @@ dag = DAG('run_main_py_in_ecr_container', default_args=default_args, schedule_in
 run_main_py = DockerOperator(
     task_id='run_main_py_in_ecr_container',
     image='957951454565.dkr.ecr.eu-west-3.amazonaws.com/ecomm-dbt-project:1.0.1',  # Replace with your ECR image and tag
-    api_version='auto',
     command='python /usr/python/dbt_run.py',  # Command to execute main.py within the container
-    #docker_url='unix://var/run/docker.sock',  # This might differ based on your setup
-    #network_mode='bridge',  # Or other network modes as needed
+    docker_conn_id='docker',
+    api_version='auto',
     dag=dag,
 )
 
