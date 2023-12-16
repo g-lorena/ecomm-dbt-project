8c56097954463353c048df3752a2c07e7fecd242
lgongang <lorena.gongang.partner@decathlon.com>
2023-12-16 22:44:10 +0100

    added docker connection id, and add docker_url

--- ecom_dag.py
+++ ecom_dag.py
@@ -16,10 +16,12 @@ run_main_py = DockerOperator(
     task_id='run_main_py_in_ecr_container',
     image='957951454565.dkr.ecr.eu-west-3.amazonaws.com/ecomm-dbt-project:1.0.1',  # Replace with your ECR image and tag
     command='python /usr/python/dbt_run.py',  # Command to execute main.py within the container
-    docker_conn_id='docker',
+    docker_conn_id='docker_registry',
     api_version='auto',
     network_mode="bridge",
     dag=dag,
+    docker_url='tcp://ec2-15-236-113-163.eu-west-3.compute.amazonaws.com:1111'
+    
 )
 
 run_main_py
